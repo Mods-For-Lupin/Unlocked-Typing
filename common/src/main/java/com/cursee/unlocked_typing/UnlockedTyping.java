17618158cdb9751fef0aef5f4f67bdfc6fdae668
jason13official <jason13official@gmail.com>
2025-10-16 12:51:42 -0400

    chat formatting mixin

--- common/src/main/java/com/cursee/unlocked_typing/UnlockedTyping.java
+++ common/src/main/java/com/cursee/unlocked_typing/UnlockedTyping.java
@@ -10,4 +10,20 @@ public class UnlockedTyping {
   public static ResourceLocation identifier(String path) {
     return new ResourceLocation(Constants.MOD_ID, path);
   }
+
+  public static void main(String[] args) {
+    System.out.println((char) 257);
+    System.out.println((char) 335);
+    System.out.println((char) 259);
+    System.out.println((char) 261);
+    System.out.println((char) 262);
+    System.out.println((char) 263);
+    System.out.println((char) 264);
+    System.out.println((char) 265);
+    System.out.println((char) 266);
+    System.out.println((char) 267);
+    System.out.println((char) 268);
+    System.out.println((char) 269);
+    System.out.println((char) 9999);
+  }
 }
--- fabric/src/main/java/com/cursee/unlocked_typing/mixin/FabricSharedConstantsMixin.java
+++ fabric/src/main/java/com/cursee/unlocked_typing/mixin/FabricSharedConstantsMixin.java
@@ -1,5 +1,6 @@
 package com.cursee.unlocked_typing.mixin;
 
+import com.cursee.unlocked_typing.platform.Services;
 import net.minecraft.SharedConstants;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +12,16 @@ public class FabricSharedConstantsMixin {
 
   @Inject(at = @At("TAIL"), method = "isAllowedChatCharacter", cancellable = true)
   private static void unlocked_typing$isAllowedChatCharacter(char character, CallbackInfoReturnable<Boolean> cir) {
+
+    if (Services.PLATFORM.isDevelopmentEnvironment()) {
+      System.out.println("typed char " + Character.toString(character) + " keycode of " + ((int) character));
+
+      if (character == 167) {
+        System.out.println("Implicit character had keycode of 167 " + Character.toString(character));
+        cir.setReturnValue(true);
+      }
+    }
+
     if (character == 167) {
       cir.setReturnValue(true);
     }

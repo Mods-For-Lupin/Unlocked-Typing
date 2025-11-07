9880f81a363ee346f0dc935338025fbc2fda4c12
jason13official <jason13official@gmail.com>
2025-11-07 13:30:11 -0500

    feat: access transforms, accessors

---
+++ common/src/main/java/com/cursee/unlocked_typing/api/client/DisplayCacheAccessor.java
@@ -0,0 +1,8 @@
+package com.cursee.unlocked_typing.api.client;
+
+import net.minecraft.client.gui.screens.inventory.BookEditScreen.LineInfo;
+
+public interface DisplayCacheAccessor {
+
+  LineInfo[] unlocked_typing$getLines();
+}
---
+++ common/src/main/java/com/cursee/unlocked_typing/api/client/LineInfoAccessor.java
@@ -0,0 +1,12 @@
+package com.cursee.unlocked_typing.api.client;
+
+import net.minecraft.network.chat.Component;
+
+public interface LineInfoAccessor {
+
+  int unlocked_typing$getX();
+
+  int unlocked_typing$getY();
+
+  Component unlocked_typing$getAsComponent();
+}

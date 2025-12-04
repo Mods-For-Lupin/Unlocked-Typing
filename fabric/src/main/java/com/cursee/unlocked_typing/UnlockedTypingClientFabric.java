38bec0cb60f4c0e6dbcbb101a216144e709e8280
jason13official <jason13official@gmail.com>
2025-12-04 13:48:53 -0500

    fix: client entrypoints

---
+++ fabric/src/main/java/com/cursee/unlocked_typing/UnlockedTypingClientFabric.java
@@ -0,0 +1,11 @@
+package com.cursee.unlocked_typing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnlockedTypingClientFabric implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {
+    UnlockedTypingClient.init();
+  }
+}
---
+++ neoforge/src/main/java/com/cursee/unlocked_typing/UnlockedTypingClientNeoForge.java
@@ -0,0 +1,8 @@
+package com.cursee.unlocked_typing;
+
+public class UnlockedTypingClientNeoForge {
+
+  public UnlockedTypingClientNeoForge() {
+    UnlockedTypingClient.init();
+  }
+}
